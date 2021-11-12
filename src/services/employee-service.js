7e11d6e4db7313c686cf8ac2a086d5895ffe4a79
Vinay288 <vinayhiremath288@gmail.com>
2021-11-12 20:42:30 +0530

    ADDED: employee services

---
+++ src/services/employee-service.js
@@ -0,0 +1,16 @@
+import config from "../config/config"
+import React, { Component } from 'react'
+
+const axios=require('axios').default;
+
+export default class EmployeeService{
+
+    baseUrl=config.baseUrl;
+    addEmployee(data)
+    {
+        console.log(data)
+        return axios.post(`${this.baseUrl}employee/`,data);
+    }
+}
+
+
