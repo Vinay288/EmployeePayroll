f561fc06546e9ec947e0b057dbd1bb314bbb4898
Vinay288 <vinayhiremath288@gmail.com>
2021-11-13 11:11:59 +0530

    ADDED:display jsx

---
+++ src/components/display/display.jsx
@@ -0,0 +1,45 @@
+import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
+import editIcon from '../../assets/icons/create-black-18dp.svg'
+import '../home/home.scss'
+
+const Display = (props) => {   
+    const remove= (id) =>{
+
+    } 
+    const update=(id)=>{
+        
+    }
+    return(
+        <table id="display" className="display">
+            <tbody>
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Department</th>
+                    <th>Salary</th>
+                    <th>Start Date</th>
+                    <th>Actions</th>
+                </tr>
+                {
+                    props.employeeArray && props.employeeArray.map((element, ind) => (
+                        <tr key={ind}>
+                            <td><img className="profile" src={element.profileUrl} alt="dd" /></td>
+                            <td>{element.name}</td>
+                            <td>{element.gender}</td>
+                            <td>{element.departMentValue && element.departMentValue.map(dept => (
+                                <div className='dept-label'>{dept}</div>))}</td>
+                                <td>$ {element.salary}</td>
+                                <td>{element.startDate}</td>
+                                <td><img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
+                                <img onClick={() => update(element.id)} src={editIcon} alt="edit" />
+                                </td>
+                                </tr>
+                
+                    ))
+                }
+            </tbody>
+        </table>
+    );
+}
+export default Display;
