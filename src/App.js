12eb65223be8518d1c72c4b31707833e8f1c984e
Vinay288 <vinayhiremath288@gmail.com>
2021-11-13 11:11:14 +0530

    REFACTORED

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import PayrollForm from './components/payroll-form/payroll-form';
+import Home from './components/home/home';
 import { Route,Routes,BrowserRouter } from 'react-router-dom'  
 function App() {
   return (
     <BrowserRouter>  
       <Routes>
       <Route path="/" element={<PayrollForm />} ></Route> 
+      <Route path="/home" element={<Home />}></Route>
       </Routes>
   </BrowserRouter>  
   );
