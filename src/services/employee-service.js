d16a2d2b28397a7df20cf313a985dc25a23e7f8f
Vinay288 <vinayhiremath288@gmail.com>
2021-11-15 17:53:45 +0530

    MODIFIED: api calls to match spring application

--- src/services/employee-service.js
+++ src/services/employee-service.js
@@ -9,26 +9,26 @@ export default class EmployeeService{
     addEmployee(data)
     {
         console.log("push data")
-        return axios.post(`${this.baseUrl}employee/`,data);
+        return axios.post(`${this.baseUrl}create/`,data);
     }
     getAllEmployees()
     {
         console.log("get data")
-        return axios.get(`${this.baseUrl}employee/`);
+        return axios.get(`${this.baseUrl}`);
     }
     deleteEmployee(id)
     {
         console.log("deleted")
-        axios.delete(`${this.baseUrl}employee/`+id);
+        axios.delete(`${this.baseUrl}delete/`+id);
     }
     updateEmployee(data,id)
     {
         console.log("update")
-        axios.put(`${this.baseUrl}employee/`+id,data);
+        axios.put(`${this.baseUrl}update/`+id,data);
     }
     getEmployee(id){
         console.log("get data for one")
-        return axios.get(`${this.baseUrl}employee/`+id);
+        return axios.get(`${this.baseUrl}get/`+id);
     }
 }
 
