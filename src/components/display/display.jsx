ff8a46233eb56c6484c5230ddbb8525d9528a64e
Vinay288 <vinayhiremath288@gmail.com>
2021-11-15 13:02:31 +0530

    ADDED: method to update

--- src/components/display/display.jsx
+++ src/components/display/display.jsx
@@ -1,19 +1,38 @@
 import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
+import React, { useState } from 'react';
 import editIcon from '../../assets/icons/create-black-18dp.svg'
 import '../home/home.scss'
 import EmployeeService from '../../services/employee-service';
+import PayrollForm from '../payroll-form/payroll-form';
+import Demo from '../home/demo';
+import { Link, useNavigate } from 'react-router-dom'
 
 var employee = new EmployeeService();
 
-const Display = (props) => {   
-    const remove= (id) =>{
-        console.log("fdfdfd")
+const Display = (props) => {
+    const navigate = useNavigate();
+    const [name, setName] = useState("Tarun");
+    const [employeeArray, setEmployeeArray] = useState([])
+    const remove = (id) => {
         employee.deleteEmployee(id)
-    } 
-    const update=(id)=>{
+        window.location.reload();
 
     }
-    return(
+    const update = (id) => {
+        console.log(id)
+        employee.getEmployee(id).then(emp => {
+            setEmployeeArray(emp.data)
+            console.log("hhh")
+            return (
+                <div>
+                    <Demo name={name} />
+                </div>);
+
+        }).catch(err => {
+            console.log("error", err)
+        })
+    }
+    return (
         <table id="display" className="display">
             <tbody>
                 <tr>
@@ -33,13 +52,13 @@ const Display = (props) => {
                             <td>{element.gender}</td>
                             <td>{element.departMentValue && element.departMentValue.map(dept => (
                                 <div className='dept-label'>{dept}</div>))}</td>
-                                <td>$ {element.salary}</td>
-                                <td>{element.startDate}</td>
-                                <td><img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
-                                <img onClick={() => update(element.id)} src={editIcon} alt="edit" />
-                                </td>
-                                </tr>
-                
+                            <td>$ {element.salary}</td>
+                            <td>{element.startDate}</td>
+                            <td><img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
+                                <Link to={"/form/" + (element.id)}> <img onClick={() => update(element.id)} src={editIcon} alt="edit" /></Link>
+                            </td>
+                        </tr>
+
                     ))
                 }
             </tbody>
